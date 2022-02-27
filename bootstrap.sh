4a62cf025b6c26c6158d27dc93dd1d35181eb0c8
Awais Memon <awaismemon26@gmail.com>
2022-02-27 14:51:52 +0100

    create bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,6 @@ DOTDIR="$(
       pwd -P
 )"
 
-BREWFILE=($DOTDIR)/brew/
-
 echo "Starting Setup"
 
 
@@ -31,34 +29,7 @@ fi
 # Update homebrew recipes
 brew update
 
-PACKAGES=(
-    git
-    tmux
-    bat
-    macvim
-    mysql
-    fzf
-    ctags
-<...list all the packages you want to install>
-    readline
-)
-echo "Installing packages..."
-brew install ${PACKAGES[@]}
-# any additional steps you want to add here
 
-echo "Installing cask..."
-CASKS=(
-    iterm2
-    adobe-acrobat-reader
-    skype
-    slack
-    spotify
-    intellij-idea-ce
-    visual-studio-code
-    steam
-    evernote
-    1password
-    macdown
-)
-echo "Installing cask apps..."
-brew cask install ${CASKS[@]}
+echo "Installing Brew bundle using Brewfile..."
+brew bundle install --file=${DOTDIR}/Brewfile
+
