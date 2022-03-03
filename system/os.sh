68c0c4b3ffedea15bbbf96185616170b6ef3ac50
Awais Memon <awaismemon@MBP.local>
2022-03-03 20:27:06 +0100

    system commands

--- system/os.sh
+++ system/os.sh
@@ -1,2 +1,8 @@
 # Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
+
+cp -Rp ~/Library/Services ~/.dotfiles/system/Library/ # automator stuff
+cp -Rp ~/Library/Fonts ~/.dotfiles/system/Library/ # all those fonts you've installed
+
+# Copy the file to specified location and restart dock by `killall Dock`
+cp -Rp ~/Library/Preferences/com.apple.dock.plist ~/.dotfiles/system/Library
