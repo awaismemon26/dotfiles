73c2428aecfeffd6c90e07fffc86af6b07480b50
Awais Memon <awaismemon26@gmail.com>
2023-06-11 13:12:38 +0200

    chore: macos settings

--- system/os.sh
+++ system/os.sh
@@ -1,20 +1,21 @@
-# Show hidden files in finder
-defaults write com.apple.finder AppleShowAllFiles YES
+## The preference you wish to set is saved in ~/Library/Preferences/ByHost, so you have to add the switch -currentHost to the defaults command
+
+#################### GENERAL 
 
 # Disable automatic capitalization as it’s annoying when typing code
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
 # Disable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write -g com.apple.swipescrolldirection -bool false 
 
 # Increase sound quality for Bluetooth headphones/headsets
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
 
 # Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -setusingnetworktime on # turn on 
 sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
 
-
-#################### ENERGY SAVING
+#################### ENERGY SAVING -- Power Management Settings
 # Enable lid wakeup
 sudo pmset -a lidwake 1
 
@@ -43,9 +44,14 @@ sudo chflags uchg /private/var/vm/sleepimage
 
 ###################### SCREEN
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+# Do not require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 60
+defaults write com.apple.screensaver askForPasswordDelay -int 120
+
+##################### SCREENSHOTS
+
+# Include date and time in screenshot filenames e.g Screenshot 2020-01-09 at 13.27.20.png
+defaults write com.apple.screencapture "include-date" -bool "true" 
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
@@ -56,17 +62,20 @@ defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
 ##################### FINDER
 
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Show hidden files in finder
+defaults write com.apple.Finder AppleShowAllFiles -bool NO 
+
+# Finder: hide all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool false
 
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
 # Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder "ShowPathbar" -bool "true"
 
 # When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
 
 # Avoid creating .DS_Store files on network or USB volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
@@ -77,10 +86,16 @@ defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
+# Default List view style
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+
 ####################### DOCK
 
+# Dock size
+defaults write com.apple.dock "tilesize" -int "45" 
+
 # Change minimize/maximize window effect
-defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock "mineffect" -string "scale" 
 
 # Minimize windows into their application’s icon
 defaults write com.apple.dock minimize-to-application -bool true
@@ -91,10 +106,9 @@ defaults write com.apple.dock show-process-indicators -bool true
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
 
+####################### TIME MACHINE
 # Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-
+defaults write com.apple.TimeMachine "DoNotOfferNewDisksForBackup" -bool "false"
 
 #################### MESSAGES
 # Prevent Photos from opening automatically when devices are plugged in
@@ -105,3 +119,7 @@ cp -Rp ~/Library/Fonts ~/.dotfiles/system/Library/ # all those fonts you've inst
 
 # Copy the file to specified location and restart dock by `killall Dock`
 cp -Rp ~/Library/Preferences/com.apple.dock.plist ~/.dotfiles/system/Library
+
+#################### Restart to see change effect
+killall Finder
+killall Dock
